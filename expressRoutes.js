38e5002a934140a49d360f6df1fd6627c2daaa19
Spencer Davis <spencerdavis226@gmail.com>
2025-03-03 15:48:14 -0500

    Added expressRoutes.js to explore app.get and req/res

--- expressRoutes.js
+++ expressRoutes.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/dogs', (req, res) => {
+  res.send('<h1>WOOF WOOF</h1>');
+});
+
 // app.listen() should always be at the bottom of your file
 app.listen(3000, () => {
   console.log('Server running on port 3000');
