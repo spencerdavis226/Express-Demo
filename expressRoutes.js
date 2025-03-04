c2b61d56fce3c5649cfb28b86d2a883f716d5f8a
Spencer Davis <spencerdavis226@gmail.com>
2025-03-04 11:29:02 -0500

    Added more examples for get/post requests. Added req.params

--- expressRoutes.js
+++ expressRoutes.js
@@ -1,12 +1,35 @@
-// Boilerplate express code
 const express = require('express');
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('HOMEPAGE');
+});
 
 app.get('/dogs', (req, res) => {
   res.send('<h1>WOOF WOOF</h1>');
 });
 
-// app.listen() should always be at the bottom of your file
+app.get('/chickens', (req, res) => {
+  res.send('BOCK! BOCK! BOCK! (get request)');
+});
+
+app.post('/chickens', function createChicken(req, res) {
+  res.send('YOU CREATED A NEW CHICKEN (post request');
+});
+
+const greetings = {
+  en: 'hello',
+  fr: 'bonjour',
+  ic: 'hallo',
+  ja: 'konnichiwa',
+};
+
+app.get('/greet/:language', (req, res) => {
+  const lang = req.params.language;
+  const greeting = greetings[lang];
+  res.send(greeting);
+});
+
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
